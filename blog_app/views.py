60c059c70fb9d4a655b2ca91af4094ec8c91e668
Brayan Tamiche <brayan.tamiche.g.dbb@gmail.com>
2023-12-02 10:31:03 -0500

    Homepage and about

---
+++ blog_app/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name="Home.html"
+
+class AboutPageView(TemplateView):
+    template_name="about.html"
